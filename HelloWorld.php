2555fa5dd261375df3e608f7520bba972d5f0ec7
dannyhma <dannyhimawan089@gmail.com>
2023-09-22 19:45:44 +0700

    update code

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,10 +1,3 @@
 <?php
 
-function sayHello(string $name, callable $filter)
-{
-    $finalName = call_user_func($filter, $name);
-    echo "Hello $finalName" . PHP_EOL;
-}
-
-sayHello("Danny", "strtoupper");
-sayHello("Danny", fn ($name) => strtoupper($name));
+echo "Hello World";
