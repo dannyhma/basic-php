713af182b4805563400dad6292e96f58f29d4057
dannyhma <dannyhimawan089@gmail.com>
2023-10-24 16:51:36 +0700

    update code

---
+++ belajar-php-oop/CommaParameterList.php
@@ -0,0 +1,24 @@
+<?php
+
+function sayHello(string $first, string $last)
+{
+}
+
+function sum(int ...$value)
+{
+}
+
+sayHello("Danny", "Himawan",);
+
+sum(
+    1,
+    1,
+    1,
+    1,
+);
+
+$array = [
+    "first" => "Danny",
+    "middle" => "D.",
+    "last" => "Himawan",
+];
---
+++ belajar-php-oop/NewStringFunctions.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump(str_contains("Danny Himawan", "Danny"));
+var_dump(str_contains("Danny Himawan", "Himawan"));
+var_dump(str_contains("Danny Himawan", "Dziky"));
+
+var_dump(str_starts_with("Danny Himawan", "Danny"));
+var_dump(str_starts_with("Danny Himawan", "Himawan"));
+var_dump(str_starts_with("Danny Himawan", "Dziky"));
+
+var_dump(str_ends_with("Danny Himawan", "Danny"));
+var_dump(str_ends_with("Danny Himawan", "Himawan"));
+var_dump(str_ends_with("Danny Himawan", "Dziky"));
