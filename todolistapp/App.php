11782bce460f7feac82d975882fa75ebfb47f297
dannyhma <dannyhimawan089@gmail.com>
2024-05-27 22:25:32 +0700

    update code in App.php

--- todolistapp/App.php
+++ todolistapp/App.php
@@ -1,14 +1,19 @@
 <?php
 
-require_once __DIR__ . "/Model/TodoList.php";
-require_once __DIR__ . "/BusinessLogic/ShowTodoList.php";
-require_once __DIR__ . "/BusinessLogic/AddTodoList.php";
-require_once __DIR__ . "/BusinessLogic/RemoveTodoList.php";
-require_once __DIR__ . "/View/ViewShowTodoList.php";
-require_once __DIR__ . "/View/ViewAddTodoList.php";
-require_once __DIR__ . "/View/ViewRemoveTodoList.php";
-require_once __DIR__ . "/Helper/Input.php";
+require_once __DIR__ . "/Entity/TodoList.php";
+require_once __DIR__ . "/Helper/inputHelper.php";
+require_once __DIR__ . "/Repository/TodoRepository.php";
+require_once __DIR__ . "/Service/TodoListService.php";
+require_once __DIR__ . "/View/TodoListView.php";
+
+use Repository\TodoListRepositoryImpl;
+use Service\TodoListServiceImpl;
+use View\TodoListView;
 
 echo "App TodoList" . PHP_EOL;
 
-viewShowTodoList();
+$todoListRepository = new TodoListRepositoryImpl();
+$todoListService = new TodoListServiceImpl($todoListRepository);
+$todoListView = new TodoListView($todoListService);
+
+$todoListView->showTodoList();
